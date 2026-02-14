e55148bb084ee886f039f3f83ef50e5fea8cd2b3
David <david@tikfinity.com>
2026-02-14 22:49:27 +0100

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -288,7 +288,7 @@
     </nav>
 
     <!-- Hero Section -->
-    <section id="calcForm" class="relative pt-32 pb-20 lg:pt-48 lg:pb-32 overflow-hidden bg-cover bg-center"
+    <section id="heroSection" class="relative pt-32 pb-20 lg:pt-48 lg:pb-32 overflow-hidden bg-cover bg-center"
         style="background-image: url('/images/hero-bg.webp');">
 
         <!-- Overlay for readability -->
